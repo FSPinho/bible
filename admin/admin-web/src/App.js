522e2f2272e9c6cfdf9b47c294986303bc42c362
Felipe Pinho <felipe76857685@gmail.com>
2018-11-08 04:31:33 -0300

    Just updating

--- admin/admin-web/src/App.js
+++ admin/admin-web/src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import ReactMde from "react-mde";
+import * as Showdown from "showdown";
+import renderHTML from 'react-render-html';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import 'react-mde/lib/styles/css/react-mde-all.css';
+import 'draft-js/dist/Draft.css'
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "**Hello world!!!**"
+        };
+        this.converter = new Showdown.Converter({
+            tables: true,
+            simplifiedAutoLink: true,
+            strikethrough: true,
+            tasklists: true
+        });
+    }
+
+    handleValueChange = (value: string) => {
+        this.setState({value});
+    };
+
+    render() {
+        return (
+            <div style={styles.page}>
+                <div style={styles.box}>
+                    <ReactMde
+                        style={styles.flex}
+                        onChange={this.handleValueChange}
+                        value={this.state.value}
+                        minEditorHeight={480}
+                        generateMarkdownPreview={markdown =>
+                            Promise.resolve(this.converter.makeHtml(markdown))
+                        }
+                    />
+                    <div style={{...styles.flex, ...styles.box}}>
+                        {renderHTML(this.converter.makeHtml(this.state.value))}
+                    </div>
+                </div>
+                <textarea value={this.state.value.replace(/\n/g, '\\n')} rows={10}/>
+            </div>
+        );
+    }
 }
 
-export default App;
+const styles = {
+    page: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        overflowY: 'auto',
+        overflowX: 'hidden',
+        backgroundColor: '#EFEFEF',
+    },
+    box: {
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 16
+    },
+    flex: {
+        flex: 1
+    }
+}
