88f9624c63e28e887c0a99091900249ce8dd6588
Felipe Pinho <felipe76857685@gmail.com>
2019-10-15 23:06:17 -0300

    Just updating

--- android/app/src/main/java/com/cytech/bible/MainApplication.java
+++ android/app/src/main/java/com/cytech/bible/MainApplication.java
@@ -16,6 +16,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.cytech.bible.BuildConfig;
 
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
